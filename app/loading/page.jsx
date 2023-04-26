c2c0a24499025ef7be6adae2161500bc010f2c8b
Hassan-KreateStudio <hassanmutebi@kreate.studio>
2023-04-26 20:31:11 +0300

    push

---
+++ app/loading/page.jsx
@@ -0,0 +1,27 @@
+// "use client";
+// import React from "react";
+// import Skeleton from "react-loading-skeleton";
+// import "react-loading-skeleton/dist/skeleton.css";
+
+// const FieldLoading = () => {
+//   return (
+//     <div className="">
+//       <nav className="flex flex-col md:flex-row md:justify-between md:items-center bg-white py-4 px-4 md:px-40 mt-20">
+//         <Skeleton width={32} height={32} />
+//         <Skeleton width={980} height={13} />
+//       </nav>
+//       <div className="mt-20 flex flex-row items-center justify-center">
+//         <div className="space-y-5 flex flex-col">
+//           <Skeleton width={376} height={58} className="ml-20" />
+//           <Skeleton height={60} width={549} />
+//           <Skeleton height={60} width={549} />
+//           <Skeleton />
+//           <Skeleton width={376} className="ml-20" />
+//           <Skeleton width={150} className="ml-48" />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default FieldLoading;
